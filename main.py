eafd20a042bedbc288693788bdaee0b0831eea14
Lee Suho <riemannulus@hitagi.moe>
2020-10-11 22:07:16 +0900

    Simple GUI

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
+from gooey import Gooey, GooeyParser
+
 from core.khala import Khala
+from core.utils import load_file_list
 
 
 def upload(filename):
@@ -17,18 +20,41 @@ def download(filename):
     khala = Khala()
     start_time = datetime.now()
     print(f"[{start_time}] Start download")
-    khala.download(filename, save_name="downloaded.exe")
+    khala.download(filename)
     end_time = datetime.now()
     print(f"[{end_time}] Finish download")
     print(f"Start: {start_time}, End: {end_time}, Record: {end_time-start_time}")
 
+@Gooey(
+    program_name='Test'
+)
+def main():
+    parser = GooeyParser(
+        description='example'
+    )
+    g = parser.add_argument_group()
+    stuff = g.add_mutually_exclusive_group(
+        required=True
+    )
+    stuff.add_argument(
+        '--upload',
+        dest='upload',
+        widget='FileChooser'
+    )
+    stuff.add_argument(
+        '--download',
+        dest='download',
+        widget='Dropdown',
+        choices=load_file_list()
+    )
 
-def main(action, filename):
-    if action == 'upload':
-        upload(filename)
-    if action == 'download':
-        download(filename)
+    args = parser.parse_args()
+
+    if args.upload is not None:
+        upload(args.upload)
+    elif args.download is not None:
+        download(args.download)
 
 
 if __name__ == '__main__':
-    main('download', 'target.exe')
+    main()
